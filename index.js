07cb44a57de8ff1723965974efad51445737792e
wesley.moody <wgm3190@gmail.com>
2019-11-11 21:05:43 -0600

    added get model years

--- index.js
+++ index.js
@@ -249,9 +249,13 @@ function sortCarInventory(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(inventory) {
-  const carYears = inventory
-}
+function getModelYears(arr) {
+  const yearArray = [];
+  for (let i = 0; i <  arr.length; i++) {
+    yearArray.push(arr[i].car_year)
+  }
+  return yearArray;
+};
 
 /**
  * ### Challenge `getOlderCars`
